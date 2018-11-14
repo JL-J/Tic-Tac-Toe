db5b1f4fdb65bcf31fcb0d26fd9648205cebcfb4
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-11-14 12:18:01 +0000

    basic jQuery is working: Can click into a box and an X appears

---
+++ public/interface.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  // PLAYER_TOKEN = "X"
+
+$('.box').click(function() {
+  $(this).html("X");
+})
+
+$('#restart').on('click', function() {
+  console.log("It works");
+})
+
+});
